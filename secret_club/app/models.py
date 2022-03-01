c7efa9599782fc443a43676b180be354bb3f26f0
Jose Venzke <josevenzkejr@gmail.com>
2022-03-01 18:39:07 -0300

    created user model

--- secret_club/app/models.py
+++ secret_club/app/models.py
@@ -1,13 +1,8 @@
 from django.db import models
-from django.forms import CharField
 
 class User(models.Model):
     name = models.CharField(max_length=20)
-    
+    image = models.ImageField()
 
-# Create your models here.
-class Comment(models.Model):
-    author = models.CharField(max_length=50)
-    text = models.TextField()
 
     
