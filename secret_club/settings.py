39332dabcdbb20388b9df992d8893af73115e29d
JoseVenzke <josevenzkejr@gmail.com>
2022-03-04 20:45:17 -0300

    fix

--- secret_club/settings.py
+++ secret_club/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yqnmjghp6wjdu=ur(e4-4$2hdu5=x8hp!d=ypg$ll2)_=bk+n' '
+SECRET_KEY = 'yqnmjghp6wjdu=ur(e4-4$2hdu5=x8hp!d=ypg$ll2)_=bk+n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
